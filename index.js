356bf54c1d030aa8a71d93c88d4248b80f9b6117
LQW <lxw06gm@gmail.com>
2020-05-27 19:05:30 -0500

    changed greeting

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express(); // generate new app
 //listen incomng request
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "there" });
 });
 
 const PORT = process.env.PORT || 5000; // inject env variable at run timeout
