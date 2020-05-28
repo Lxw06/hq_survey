48993d3c64baae1db704041d1034739c93dcae1c
LQW <lxw06gm@gmail.com>
2020-05-28 15:05:06 -0500

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+// prod keys auth_server
+module.exports = {
+  //Smaily-dev
+  googleClientID: process.env.GOOGL_CLIENT_ID,
+
+  googleClientSecret: process.env.GOOGL_CLIENT_SECRET,
+  // project 0 for test purpose, free
+  mongoURI: process.env.MOMGO_URI,
+
+  cookieKey: process.env.COOKIE_KEY
+};
