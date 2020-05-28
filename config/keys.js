35765049cc8c75597e1c6bc91d1631f2b3bb27a0
LQW <lxw06gm@gmail.com>
2020-05-28 15:25:33 -0500

    change from === to == in keys file

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // keys.js-- figure out what set of cerdentials t return
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV == "production") {
   module.exports = require("/prod");
 } else {
   module.exports = require("/dev");
